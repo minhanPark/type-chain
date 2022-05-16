70d051a72541c46b911794ac7ea12aaca2f971b2
박민한 <alsgks1218@naver.com>
2022-05-16 10:23:57 +0900

    course completed

--- build/index.js
+++ build/index.js
@@ -1,1 +1,40 @@
-var greeting = function (name) { return "".concat(name, "\uB2D8 \uC548\uB155\uD558\uC138\uC694."); };
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+class BlockChain {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevHash() {
+        if (this.blocks.length === 0)
+            return "genesis block";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+}
+const blockchain = new BlockChain();
+blockchain.addBlock("FIrst");
+blockchain.addBlock("two");
+blockchain.addBlock("threee");
+console.log(blockchain.getBlocks());
--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,42 @@ class Block implements BlockShape {
   constructor(
     public prevHash: string,
     public height: number,
-    public data: number
+    public data: string
   ) {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "genesis block";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new BlockChain();
+
+blockchain.addBlock("FIrst");
+blockchain.addBlock("two");
+blockchain.addBlock("threee");
+
+console.log(blockchain.getBlocks());
